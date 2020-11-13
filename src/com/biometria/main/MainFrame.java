916b0b3752498cb59d1ce9163d680cae07a2fe4e
Lucas Bortolo <lucasbortolo3@gmail.com>
2020-11-13 10:50:30 -0300

    ocultando instanciamento de teste em MainFrame

--- src/com/biometria/main/MainFrame.java
+++ src/com/biometria/main/MainFrame.java
@@ -84,8 +84,8 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        DadosFrame dados = new DadosFrame();
-        dados.setVisible(true);
+        //DadosFrame dados = new DadosFrame();
+        //dados.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
